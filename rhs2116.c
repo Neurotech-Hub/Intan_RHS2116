adf614e02ae59bace1128398cf9b1fa5150ea1ba
Matt Gaidica, Ph.D <matt@gaidi.ca>
2024-02-13 04:54:53 -0600

    Update r/w functions

--- rhs2116.c
+++ rhs2116.c
@@ -4,6 +4,7 @@
  ******************************************************************************/
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include "rhs2116.h"
 #include "spidrv.h"
 
@@ -34,35 +35,80 @@ void rhs2116_init(SPIDRV_Handle_t spiHandle) {
 	rhs2116_context.spiHandle = spiHandle;
 }
 
-void rhs2116_writeRegister(uint8_t regAddress, uint32_t regValue) {
-	uint32_t command = (regAddress << 24) | regValue;
-	transfer_complete = false; // Reset the completion flag before starting the transfer
+bool rhs2116_writeRegister(uint8_t regAddress, uint16_t regValue, bool uFlag, bool mFlag) {
+    // Clear the buffers
+    tx_buffer = 0;
+    rx_buffer = 0;
 
-	// Initiate the transfer, reuse regValue (cannot be NULL)
-	Ecode_t ecode = SPIDRV_MTransfer(rhs2116_context.spiHandle, &command,
-			&regValue, sizeof(command), transfer_callback);
-	EFM_ASSERT(ecode == ECODE_EMDRV_SPIDRV_OK);
+    // Split regValue into MSB and LSB
+    uint8_t dataMSB = (regValue >> 8) & 0xFF; // Extract MSB of the data
+    uint8_t dataLSB = regValue & 0xFF;       // Extract LSB of the data
 
-	// Wait for the transfer to complete
-	while (!transfer_complete)
-		;
+    // Construct the LSB byte of the command with the specified structure
+    uint8_t lsbCommand = 0x80; // Bit 7 set to 1, bits 6-0 set to 0 as base
+    if (uFlag) {
+        lsbCommand |= 0x20; // Set U flag (bit 5)
+    }
+    if (mFlag) {
+        lsbCommand |= 0x10; // Set M flag (bit 4)
+    }
+
+    // Assemble the command with dataLSB in the most significant position
+    tx_buffer = ((uint32_t)dataLSB << 24) | ((uint32_t)dataMSB << 16) | ((uint32_t)regAddress << 8) | lsbCommand;
+
+    transfer_complete = false; // Reset the completion flag before starting the transfer
+
+    // Initiate the transfer using global buffers
+    Ecode_t ecode = SPIDRV_MTransfer(rhs2116_context.spiHandle, &tx_buffer, &rx_buffer, sizeof(tx_buffer), transfer_callback);
+    EFM_ASSERT(ecode == ECODE_EMDRV_SPIDRV_OK);
+
+    // Wait for the transfer to complete
+    while (!transfer_complete)
+        ;
+
+    // Extract the last 2 bytes from rx_buffer
+    uint16_t receivedValue = (uint16_t)((rx_buffer >> 8) & 0xFFFF);
+
+    // Check if the received value matches the sent value
+    if (receivedValue == regValue) {
+        return true; // Data integrity check passed
+    } else {
+        return false; // Data integrity check failed
+    }
 }
 
-uint32_t rhs2116_readRegister(uint8_t regAddress) {
-	uint32_t command = (0x80000000 | (regAddress << 24));
-	uint32_t regValue = 0;
-	transfer_complete = false; // Reset the completion flag before starting the transfer
+uint16_t rhs2116_readRegister(uint8_t regAddress, bool uFlag, bool mFlag) {
+    // Clear the buffers
+    tx_buffer = 0;
+    rx_buffer = 0;
 
-	// Initiate the transfer to send the command
-	Ecode_t ecode = SPIDRV_MTransfer(rhs2116_context.spiHandle, &command,
-			&regValue, sizeof(command), transfer_callback);
-	EFM_ASSERT(ecode == ECODE_EMDRV_SPIDRV_OK);
+    // For read commands, we set bit 6 to 1 and keep bit 7 as 1 to differentiate from write commands.
+    uint8_t lsbCommand = 0xC0; // Bits 7 and 6 set to 1, bits 5-0 set to 0 as base
+    if (uFlag) {
+        lsbCommand |= 0x20; // Set U flag (bit 5)
+    }
+    if (mFlag) {
+        lsbCommand |= 0x10; // Set M flag (bit 4)
+    }
 
-	// Wait for the transfer to complete
-	while (!transfer_complete)
-		;
+    // Assemble the command, notice that regValue is not a parameter for read operations,
+    // so we don't split it into MSB and LSB. The last byte (lsbCommand) has bit 6 set to indicate a read operation.
+    tx_buffer = ((uint32_t)regAddress << 24) | ((uint32_t)lsbCommand << 16);
 
-	return regValue;
+    transfer_complete = false; // Reset the completion flag before starting the transfer
+
+    // Initiate the transfer using global buffers. For read operations, we expect to receive data in rx_buffer.
+    Ecode_t ecode = SPIDRV_MTransfer(rhs2116_context.spiHandle, &tx_buffer, &rx_buffer, sizeof(tx_buffer), transfer_callback);
+    EFM_ASSERT(ecode == ECODE_EMDRV_SPIDRV_OK);
+
+    // Wait for the transfer to complete
+    while (!transfer_complete)
+        ;
+
+    // Extract the data from the received buffer. Assuming the data is in the lower 2 bytes of rx_buffer.
+    uint16_t receivedData = (uint16_t)(rx_buffer & 0xFFFF);
+
+    return receivedData;
 }
 
 //void spidrv_app_process_action(void)
